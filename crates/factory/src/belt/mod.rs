06a50abb04ffe8db6dd181b497673a6fd9df437d
Natalie Baker <technology@notvery.moe>
2024-02-06 05:49:20 +0800

    WIP

--- crates/factory/src/belt/mod.rs
+++ crates/factory/src/belt/mod.rs
@@ -6,8 +6,13 @@ pub const BELT_DIST_MAX:  u8 = u8::MAX;
 pub const BELT_ITEM_SIZE: u8 = 4;
 pub const BELT_ITEM_MAX:  u8 = BELT_DIST_MAX / BELT_ITEM_SIZE;
 
+pub struct BeltTrackEntry {
+    next:     u8,
+    distance: u8,
+    stack:    ItemStack,
+}
+
 pub struct BeltTrack {
-    next:      [       u8; BELT_ITEM_MAX as usize],
-    distances: [       u8; BELT_ITEM_MAX as usize],
-    items:     [ItemStack; BELT_ITEM_MAX as usize],
+    data: Vec<BeltTrackEntry>,
+    freelist: Vec<u8>,
 }
