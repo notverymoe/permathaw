3433185ba5d5bd8ed0d1f31c51cefa01e07e3f5a
Natalie Baker <technology@notvery.moe>
2024-02-09 16:00:06 +0800

    WIP

--- crates/factory/src/track/queue.rs
+++ crates/factory/src/track/queue.rs
@@ -1,13 +1,65 @@
 // Copyright 2024 Natalie Baker // AGPLv3 //
 
+use bevy::prelude::Component;
+
+#[derive(Clone, Copy, Component)]
 pub struct TrackQueue(u64);
 
 impl TrackQueue {
 
-    pub fn advance(&mut self) {
-        self.0 = accumulate_zeros_to_right(0);
+    #[must_use]
+    pub const fn next(&self) -> Self {
+        Self(accumulate_zeros_to_right(self.0))
     }
 
+    pub fn advance(&mut self) {
+        *self = self.next();
+    }
+
+    #[must_use]
+    pub const fn get(&self, idx: usize) -> bool {
+        (self.0 >> idx) & 1 == 0   
+    }
+
+    pub fn remove(&mut self, idx: usize) {
+        self.0 |= 1 << idx;
+    }
+
+    pub fn insert(&mut self, idx: usize) {
+        self.0 &= !(1 << idx);
+    }
+
+}
+
+impl Default for TrackQueue {
+    fn default() -> Self {
+        Self(u64::MAX)
+    }
+}
+
+impl core::fmt::Debug for TrackQueue {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_tuple("TrackQueue").field(&format!("{:#064b}", self.0)).finish()
+    }
+}
+
+struct TrackQueueIter<'a> {
+    queue: &'a TrackQueue,
+    idx:   usize,
+}
+
+impl<'a> Iterator for TrackQueueIter<'a> {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.idx >= 64 { return None; }
+            let idx = self.idx;
+            self.idx += 1;
+            if self.queue.get(idx) {
+                return Some(idx);
+            }
+        }
+    }
 }
 
 // ////////////////// //
@@ -100,7 +152,10 @@ const fn accumulate_zeros_to_right(v: u64) -> u64 {
     //
     let blsr = v & (v.wrapping_sub(1));
     let shr  = blsr >> 1;
-    let msb  = if v == 0 { U64_MSB } else { 0 };
+    // OPT this if statement gets compiled down to bit 
+    // ops, it just has better debug asm this way. Since
+    // multiply needs a panic handler.
+    let msb  = if v != 0 { U64_MSB } else { 0 }; 
     msb | shr
 }
 
@@ -113,7 +168,23 @@ mod test {
     use super::*;
 
     #[test]
-    pub fn test_shifting() {
+    pub fn test_queue() {
+        let mut queue = TrackQueue::default();
+        assert_eq!(queue.0, u64::MAX);
+
+        queue.insert(0);
+        assert_eq!(queue.0, !1);
+        queue.remove(0);
+        assert_eq!(queue.0, u64::MAX);
+
+        queue.insert(63);
+        assert_eq!(queue.0, !U64_MSB);
+        queue.remove(63);
+        assert_eq!(queue.0, u64::MAX);
+    }
+
+    #[test]
+    pub fn test_accumulate_zeros_to_right() {
 
         let test_set = [
             [0b0000,           0b0000],
