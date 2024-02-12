a382df1ed402c4616d0528633d2ca975ff085e55
Natalie Baker <natalie.baker@advancednavigation.com>
2024-02-12 14:50:59 +0800

    Make TrackQueue immutable

--- crates/factory/src/track/queue.rs
+++ crates/factory/src/track/queue.rs
@@ -10,25 +10,23 @@ pub struct TrackQueue(u64);
 impl TrackQueue {
 
     #[must_use]
-    pub const fn next(&self) -> Self {
+    pub const fn next(self) -> Self {
         Self(accumulate_zeros_to_right(self.0))
     }
 
-    pub fn advance(&mut self) {
-        *self = self.next();
-    }
-
     #[must_use]
-    pub const fn get(&self, idx: usize) -> bool {
+    pub const fn has(self, idx: usize) -> bool {
         (self.0 >> idx) & 1 == 0   
     }
 
-    pub fn remove(&mut self, idx: usize) {
-        self.0 |= 1 << idx;
+    #[must_use]
+    pub const fn without(self, idx: usize) -> Self {
+        Self(self.0 | (1 << idx))
     }
 
-    pub fn insert(&mut self, idx: usize) {
-        self.0 &= !(1 << idx);
+    #[must_use]
+    pub const fn with(self, idx: usize) -> Self {
+        Self(self.0 & !(1 << idx))
     }
 
 }
@@ -53,11 +51,12 @@ struct TrackQueueIter<'a> {
 impl<'a> Iterator for TrackQueueIter<'a> {
     type Item = usize;
     fn next(&mut self) -> Option<Self::Item> {
+        // TODO OPT
         loop {
             if self.idx >= 64 { return None; }
             let idx = self.idx;
             self.idx += 1;
-            if self.queue.get(idx) {
+            if self.queue.has(idx) {
                 return Some(idx);
             }
         }
@@ -94,53 +93,53 @@ mod test {
         assert_eq!(queue, conveyor_mask([]));
 
         // Insert at front
-        queue.insert(0);
+        queue = queue.with(0);
         assert_eq!(queue, conveyor_mask([0]));
-        queue.remove(0);
+        queue = queue.without(0);
         assert_eq!(queue, conveyor_mask([]));
 
         // Insert at back
-        queue.insert(63);
+        queue = queue.with(63);
         assert_eq!(queue, conveyor_mask([63]));
-        queue.remove(63);
+        queue = queue.without(63);
         assert_eq!(queue, conveyor_mask([]));
 
         // Check advance blocked 1
-        queue.insert(0);
-        queue.advance();
+        queue = queue.with(0);
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([0]));
-        queue.remove(0);
+        queue = queue.without(0);
         assert_eq!(queue, conveyor_mask([]));
 
         // Check advance blocked 2
-        queue.insert(0);
-        queue.insert(1);
-        queue.advance();
+        queue = queue.with(0);
+        queue = queue.with(1);
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([1, 0]));
-        queue.remove(0);
+        queue = queue.without(0);
         assert_eq!(queue, conveyor_mask([1]));
-        queue.advance();
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([0]));
-        queue.remove(0);
+        queue = queue.without(0);
         assert_eq!(queue, conveyor_mask([]));
 
         // Check advance unblocked 1
-        queue.insert(63);
-        queue.advance();
+        queue = queue.with(63);
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([62]));
-        queue.remove(62);
+        queue = queue.without(62);
         assert_eq!(queue, conveyor_mask([]));
 
         // Check advance unblocked 2
-        queue.insert(63);
-        queue.insert(62);
-        queue.advance();
+        queue = queue.with(63);
+        queue = queue.with(62);
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([62, 61]));
-        queue.remove(61);
+        queue = queue.without(61);
         assert_eq!(queue, conveyor_mask([62]));
-        queue.advance();
+        queue = queue.next();
         assert_eq!(queue, conveyor_mask([61]));
-        queue.remove(61);
+        queue = queue.without(61);
         assert_eq!(queue, conveyor_mask([]));
     }
 }
