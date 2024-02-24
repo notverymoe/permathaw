63bfd55eab297dbb564b590b83765cdd0696ecbe
Natalie Baker <technology@notvery.moe>
2024-02-24 12:28:14 +0800

    Move lints into workspace config

--- crates/factory_dbg/src/main.rs
+++ crates/factory_dbg/src/main.rs
@@ -1,48 +1,5 @@
 // Copyright 2024 Natalie Baker // AGPLv3 //
 
-#![warn(
-    clippy::all, 
-    clippy::pedantic,
-    clippy::alloc_instead_of_core,
-    clippy::as_underscore,
-    clippy::clone_on_ref_ptr,
-    clippy::create_dir,
-    clippy::empty_structs_with_brackets,
-    clippy::error_impl_error,
-    clippy::exit,
-    clippy::filetype_is_file,
-    clippy::fn_to_numeric_cast_any,
-    clippy::format_push_string,
-    clippy::if_then_some_else_none,
-    clippy::mixed_read_write_in_expression,
-    clippy::panic_in_result_fn,
-    clippy::partial_pub_fields,
-    clippy::unseparated_literal_suffix,
-    clippy::shadow_unrelated,
-    clippy::std_instead_of_core,
-    clippy::str_to_string,
-    clippy::string_to_string,
-    clippy::tests_outside_test_module,
-    clippy::undocumented_unsafe_blocks,
-    clippy::unnecessary_safety_comment,
-    clippy::unnecessary_safety_doc,
-    clippy::unwrap_in_result,
-    clippy::missing_const_for_fn,
-)]
-
-#![allow(
-    clippy::missing_docs_in_private_items,
-    clippy::module_name_repetitions,
-    clippy::cast_possible_truncation,
-    clippy::cast_lossless,
-    clippy::cast_sign_loss,
-    clippy::cast_precision_loss,
-    clippy::cast_possible_wrap,
-    clippy::missing_errors_doc,
-    clippy::needless_pass_by_value, // Bevy systems need this
-    // clippy::shadow_unrelated,       // Egui lambda params
-)]
-
 use bevy::prelude::*;
 use nvm_factory_dbg::{render_debug_conveyors, ConveyorPath};
 use nvm_factory_sim::{item::ItemStack, plugin::PluginsFactory, tick::{TickPacer, TickRate1}, track::{TrackBuffer, TrackPassthrough, TrackQueue}};
@@ -163,6 +120,4 @@ fn setup(mut commands: Commands) {
     commands.entity(id_b).insert(TrackPassthrough::new(id_a, 59));
     commands.entity(id_c).insert(TrackPassthrough::new(id_b, 59));
     commands.entity(id_d).insert(TrackPassthrough::new(id_c, 59));
-
-
 }
